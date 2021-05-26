6dae7d6a41b6c373c2c75535adbfdd17af99cdc5
toprakkoc <hello@toprak.dev>
2021-05-27 00:44:53 +0300

    landing page responsive design

--- src/Containers/Landing/styled.js
+++ src/Containers/Landing/styled.js
@@ -4,6 +4,17 @@ export const ContentWrapper = styled.div`
   display: flex;
   height: 65vh;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    display: flex !important;
+    height: auto !important;
+    flex-direction: column !important;
+
+    img {
+      margin: 0 !important;
+      width: 100% !important;
+    }
+  }
 `;
 
 export const Content = styled.div`
@@ -24,5 +35,22 @@ export const Content = styled.div`
     margin-top: 25px;
     margin-left: 10em;
     padding: 15px 25px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex !important;
+    flex-direction: column !important;
+    width: 100% !important;
+    align-items: center !important;
+    margin-top: 35px !important;
+    margin-left: 0 !important;
+
+    span {
+      font-size: 7.5vw !important;
+    }
+
+    button {
+      margin-left: 0;
+    }
   }
 `;
