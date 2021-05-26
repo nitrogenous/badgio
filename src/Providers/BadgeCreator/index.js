3bfd9a62f52457a9f4337a776bf8231da48e335e
toprakkoc <hello@toprak.dev>
2021-05-27 02:24:11 +0300

    temporary mobile fix

--- src/Providers/BadgeCreator/index.js
+++ src/Providers/BadgeCreator/index.js
@@ -35,7 +35,7 @@ const BadgeCreatorProvider = ({ children }) => {
         context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
         createContextTexts(context, userDetails);
       };
-      imageObj.crossOrigin = 'anonymous';
+      // imageObj.crossOrigin = 'anonymous';
       imageObj.src = canvasImage; 
     }
   };
